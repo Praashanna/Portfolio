4d300862a1c058e83b5b8043353f6a444e8b3ad8
ServerBoys <pt70046@gmail.com>
2024-04-07 20:03:50 +0545

    Refinements

---
+++ public/build/assets/ImageDescription-DwEOeAGo.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-Bzq0dP1H.js";const o=({image:a,alt:n,children:t,className:c,reverse:s=!1})=>{const e=i.jsx("div",{className:"image-container",children:i.jsx("img",{src:a,alt:n,width:"2048px",height:"1362px"})});return i.jsxs("div",{className:`image-description ${c}`,children:[!s&&e,i.jsx("div",{className:"description-container",children:t}),s&&e]})};export{o as I};
---
+++ ssh.cmd
@@ -0,0 +1,1 @@
+ssh prashannat.com.np@45.8.227.145
